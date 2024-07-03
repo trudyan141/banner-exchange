433672c9851fc4d63b0e0eaf56417a3197fc01f7
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-07-03 23:58:20 +0700

    feat: update source banner exchange render from div id

--- src/components/BEC.tsx
+++ src/components/BEC.tsx
@@ -1,7 +1,7 @@
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styles from './BEC.module.scss';
-function BEC({ width, height, walletAddress, imgUrl, externalLink }: any) {
+function BEC({ }: any) {
   /***
    * Emit event
    */
@@ -13,27 +13,74 @@ function BEC({ width, height, walletAddress, imgUrl, externalLink }: any) {
   /***
    * STATES
    */
-  const [bannerLink, setBannerLink] = useState('');
+  const [walletAddress, setWalletAddress] = useState<any>(null);
+  const [banners, setBanners] = useState<any>([]);
+
   /***
    * FUNCTIONS
    */
-  const initBannerLink = () => {
-    const rs = `${externalLink}?walletAddress=${walletAddress}`;
-    setBannerLink(rs);
+  const getCurrentScript = () => {
+  const scriptElements: any = document.getElementsByTagName('script');
+  for (let script of scriptElements) {
+    if (script.src.includes('bec.js')) {
+      return script;
+    }
   }
+  return null;
+};
+  const getWalletAddressSrc = useCallback(() => {
+    const currentScript = getCurrentScript();
+    const scriptUrl = currentScript.src;
+    if (scriptUrl) {
+      const url = new URL(scriptUrl);
+      const params = new URLSearchParams(url.search);
+      const walletAddressSrc = params.get('walletAddress');
+      console.log("🚀 ~ getWalletAddressSrc ~ walletAddressSrc:", walletAddressSrc)
+      setWalletAddress(walletAddressSrc);
+    }
+  }, [])
+  const initBanners = useCallback(() => {
+    try {
+      // TODO call API here
+      let rs = [];
+      for (let index = 0; index < 10; index++) {
+        const element = {
+          imgUrl: 'https://i.ibb.co/5WbrnBC/te-320x50.png',
+          bannerLink: 'https://sdk-web-app-dev.dmtp.tech/',
+        }
+        rs.push(element);
+      }
+      setBanners(rs);
+    } catch (error) {
+      console.log('error', error);
+    }
+  },[])
+  const initOptions = useCallback(() => {
+    getWalletAddressSrc();
+    initBanners();
+  },[getWalletAddressSrc, initBanners])
+  /**
+   * USE EFFECTS
+   */
   useEffect(() => {
-    initBannerLink();
-  }, []);
+    initOptions();
+  }, [initOptions]);
   return (
     <div className={styles.becComponent}>
       {/* <button onClick={emitEvent}> Emit event  </button> */}
-      <div className="banner-exchange-code" style={{ width: width + 'px', height: height + 'px'}}> 
-        <a href={bannerLink} target="_blank">  
-          <div className="banner-image">  
-              <img src={imgUrl} alt="banner" />
+      {banners.map((banner: any, index: any) => {
+        const { imgUrl, bannerLink } = banner;
+        return (
+          <div className="banner-exchange-code"> 
+            <div className="banner-image">  
+              <a href={bannerLink} target="_blank">  
+                <img src={imgUrl} alt="banner" />
+              </a>
+            </div>
           </div>
-        </a>
-      </div>
+        );
+      })}
+      
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,33 @@
-import r2wc from "@r2wc/react-to-web-component";
+//import r2wc from "@r2wc/react-to-web-component";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import BEC from './components/BEC';
 import './index.css';
 import { reportWebVitals } from './reportWebVitals';
+const renderBanner = () => {
+  const bannerEle = document.getElementById('exchangeBanner');
+  console.log("🚀 ~ document:", document)
+  console.log("🚀 ~ bannerEle:", bannerEle)
 
-const becWidget = r2wc(BEC, { props: { width : 'string', height : 'string', walletAddress : 'string', imgUrl : 'string', externalLink : 'string' } });
+  if (bannerEle) {
+    const root = ReactDOM.createRoot(
+      bannerEle as HTMLElement,
+    );
+    root.render(
+      <React.StrictMode>
+        <BEC />
+      </React.StrictMode>,
+    );
+  }
+}
 
-customElements.define("banner-exchange-code", becWidget);
 
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement,
-// );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// );
+document.addEventListener('DOMContentLoaded', renderBanner);
+// NOT USE THIS
+// create web component
+//const becWidget = r2wc(BEC, { props: { width : 'string', height : 'string', walletAddress : 'string', imgUrl : 'string', externalLink : 'string' } });
+//customElements.define("banner-exchange-code", becWidget);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
